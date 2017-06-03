45b90b4ac7754b0488be321f39c33f9099392af0
abiduzz420 <abiduzair420@gmail.com>
2017-06-03 07:20:54 +0530

    dropdown ui separated from App

--- uzair/ui-elements/src/App.js
+++ uzair/ui-elements/src/App.js
@@ -1,60 +1,18 @@
 import React, { Component } from 'react';
+
 import TabContainer from './TabContainer';
+import DropdownContainer from './DropdownContainer';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-        {/* <TabContainer /> */}
+        <TabContainer />
         <DropdownContainer />
       </div>
     );
   }
 }
 
-class DropdownContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isClicked: false
-    };
-  }
-
-  onClick = () => {
-    this.setState({
-      isClicked: !this.state.isClicked
-    });
-  };
-  render() {
-    return (
-      <div style={{ display: 'block' }}>
-        <DropdownTrigger onSelect={() => this.onClick()} />
-        <DropdownItems visible={this.state.isClicked} />
-      </div>
-    );
-  }
-}
-
-class DropdownTrigger extends Component {
-  render() {
-    return <div onClick={this.props.onSelect}>Dropdown Menu▼</div>;
-  }
-}
-
-class DropdownItems extends Component {
-  render() {
-    if (this.props.visible) {
-      return (
-        <div>
-          <div>DropdownItems</div>
-          <div>DropdownItems</div>
-          <div>DropdownItems</div>
-        </div>
-      );
-    } else {
-      return <div />;
-    }
-  }
-}
 export default App;
---
+++ uzair/ui-elements/src/DropdownContainer.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+class DropdownContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isClicked: false
+    };
+  }
+
+  onClick = () => {
+    this.setState({
+      isClicked: !this.state.isClicked
+    });
+  };
+  render() {
+    return (
+      <div style={{ display: 'block' }}>
+        <DropdownTrigger onSelect={() => this.onClick()} />
+        <DropdownItems visible={this.state.isClicked} />
+      </div>
+    );
+  }
+}
+
+class DropdownTrigger extends Component {
+  render() {
+    return <div onClick={this.props.onSelect}>Dropdown Menu▼</div>;
+  }
+}
+
+class DropdownItems extends Component {
+  render() {
+    if (this.props.visible) {
+      return (
+        <div>
+          <div>DropdownItems</div>
+          <div>DropdownItems</div>
+          <div>DropdownItems</div>
+        </div>
+      );
+    } else {
+      return <div />;
+    }
+  }
+}
+
+export default DropdownContainer;
