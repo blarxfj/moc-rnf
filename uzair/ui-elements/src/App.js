959bb2ba8eb86e9edfb1eff8206032d02652c48e
abiduzz420 <abiduzair420@gmail.com>
2017-06-03 06:53:16 +0530

    static Tab ui built

--- uzair/ui-elements/src/App.js
+++ uzair/ui-elements/src/App.js
@@ -1,11 +1,48 @@
 import React, { Component } from 'react';
 import './App.css';
-import TabContainer from './Tab';
 
 class App extends Component {
   render() {
-    return <div />;
+    return (
+      <div>
+        <TabContainer />
+      </div>
+    );
   }
 }
 
+class TabContainer extends Component {
+  render() {
+    return (
+      <div>
+        <div style={{ display: 'flex' }}>
+          <Tab />
+          <Tab />
+          <Tab />
+        </div>
+        <TabContent />
+      </div>
+    );
+  }
+}
+
+class Tab extends Component {
+  render() {
+    return (
+      <div>
+        Tab Number
+      </div>
+    );
+  }
+}
+
+class TabContent extends Component {
+  render() {
+    return (
+      <div>
+        This is TabContent Container
+      </div>
+    );
+  }
+}
 export default App;
