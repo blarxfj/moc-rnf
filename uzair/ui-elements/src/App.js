bba3a9f2e33edd9683412bf48ac92ba84d9bc9e7
abiduzz420 <abiduzair420@gmail.com>
2017-06-03 07:04:42 +0530

    dropdown static version built

--- uzair/ui-elements/src/App.js
+++ uzair/ui-elements/src/App.js
@@ -6,10 +6,39 @@ class App extends Component {
   render() {
     return (
       <div>
-        <TabContainer />
+        {/* <TabContainer /> */}
+        <DropdownContainer />
       </div>
     );
   }
 }
 
+class DropdownContainer extends Component {
+  render() {
+    return (
+      <div style={{ display: 'block' }}>
+        <DropdownTrigger />
+        <DropdownItems />
+      </div>
+    );
+  }
+}
+
+class DropdownTrigger extends Component {
+  render() {
+    return <div>Dropdown Menu▼</div>;
+  }
+}
+
+class DropdownItems extends Component {
+  render() {
+    return (
+      <div>
+        <div>DropdownItems</div>
+        <div>DropdownItems</div>
+        <div>DropdownItems</div>
+      </div>
+    );
+  }
+}
 export default App;
