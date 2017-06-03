cb6343a394553a2af8d154e51e9d1868aad68330
abiduzz420 <abiduzair420@gmail.com>
2017-06-03 06:58:10 +0530

    TabContainer component separated form App

--- uzair/ui-elements/src/App.js
+++ uzair/ui-elements/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import TabContainer from './TabContainer';
 import './App.css';
 
 class App extends Component {
@@ -11,76 +12,4 @@ class App extends Component {
   }
 }
 
-class TabContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedTab: 0,
-      displayMessage: ''
-    };
-  }
-
-  update = (id, tabInfo) => {
-    if (this.state.selectedTab === id) {
-      this.setState({
-        selectedTab: 0,
-        displayMessage: ''
-      });
-    } else {
-      this.setState({
-        selectedTab: id,
-        displayMessage: tabInfo[id - 1]
-      });
-    }
-  };
-
-  render() {
-    const tabInfo = ['Tab 1 is Active', 'Tab 2 is Active', 'Tab 3 is Active'];
-    return (
-      <div>
-        <div style={{ display: 'flex' }}>
-          <Tab
-            id={1}
-            name="Tab 1"
-            selectedTab={this.state}
-            onSelect={() => this.update(1, tabInfo)}
-          />
-          <Tab
-            id={2}
-            name="Tab 2"
-            selectedTab={this.state}
-            onSelect={() => this.update(2, tabInfo)}
-          />
-          <Tab
-            id={3}
-            name="Tab 3"
-            selectedTab={this.state}
-            onSelect={() => this.update(3, tabInfo)}
-          />
-        </div>
-        <TabContent message={this.state.displayMessage} />
-      </div>
-    );
-  }
-}
-
-class Tab extends Component {
-  render() {
-    return (
-      <div style={{ marginRight: '4px' }} onClick={this.props.onSelect}>
-        {this.props.name}
-      </div>
-    );
-  }
-}
-
-class TabContent extends Component {
-  render() {
-    return (
-      <div>
-        {this.props.message}
-      </div>
-    );
-  }
-}
 export default App;
