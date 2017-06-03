d2e0f99985d3e0c0191e875a001be9eac579f39e
abiduzz420 <abiduzair420@gmail.com>
2017-06-03 06:55:23 +0530

    tab interactivity added

--- uzair/ui-elements/src/App.js
+++ uzair/ui-elements/src/App.js
@@ -12,15 +12,53 @@ class App extends Component {
 }
 
 class TabContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedTab: 0,
+      displayMessage: ''
+    };
+  }
+
+  update = (id, tabInfo) => {
+    if (this.state.selectedTab === id) {
+      this.setState({
+        selectedTab: 0,
+        displayMessage: ''
+      });
+    } else {
+      this.setState({
+        selectedTab: id,
+        displayMessage: tabInfo[id - 1]
+      });
+    }
+  };
+
   render() {
+    const tabInfo = ['Tab 1 is Active', 'Tab 2 is Active', 'Tab 3 is Active'];
     return (
       <div>
         <div style={{ display: 'flex' }}>
-          <Tab />
-          <Tab />
-          <Tab />
+          <Tab
+            id={1}
+            name="Tab 1"
+            selectedTab={this.state}
+            onSelect={() => this.update(1, tabInfo)}
+          />
+          <Tab
+            id={2}
+            name="Tab 2"
+            selectedTab={this.state}
+            onSelect={() => this.update(2, tabInfo)}
+          />
+          <Tab
+            id={3}
+            name="Tab 3"
+            selectedTab={this.state}
+            onSelect={() => this.update(3, tabInfo)}
+          />
         </div>
-        <TabContent />
+        <TabContent message={this.state.displayMessage} />
       </div>
     );
   }
@@ -29,8 +67,8 @@ class TabContainer extends Component {
 class Tab extends Component {
   render() {
     return (
-      <div>
-        Tab Number
+      <div style={{ marginRight: '4px' }} onClick={this.props.onSelect}>
+        {this.props.name}
       </div>
     );
   }
@@ -40,7 +78,7 @@ class TabContent extends Component {
   render() {
     return (
       <div>
-        This is TabContent Container
+        {this.props.message}
       </div>
     );
   }
