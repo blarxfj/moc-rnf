ba94f7193704c3f603bbb6766a24f8c54a717ce5
Kacppian <aspkaushik@gmail.com>
2017-06-06 21:28:54 +0530

    Click anywhere, close the dropdown.

--- kaushik/ui-elements/src/Dropdown.js
+++ kaushik/ui-elements/src/Dropdown.js
@@ -22,7 +22,7 @@ const dropdownElementNames = [
 class DropdownElement extends Component {
   render() {
     return (
-      <div>
+      <div style={this.props.style}>
         {this.props.name}
       </div>
     );
@@ -38,14 +38,34 @@ class Dropdown extends Component {
     };
   }
 
-  onClick = () => {
+  onClick = e => {
+    e.preventDefault();
     this.setState({
       isClicked: !this.state.isClicked
     });
+    // e.target.focus();
+    this.button_focus.focus();
+    // console.log(DOMEventTarget);
+    // console.log(document.activeElement);
+    // console.log(this.button_focus);
+  };
+
+  onBlur = e => {
+    e.preventDefault();
+    this.setState({
+      isClicked: true
+    });
+    this.button_focus.blur();
+    // console.log(document.activeElement);
+    // console.log(this.button_focus);
   };
 
   render() {
     var Elements = [];
+
+    var style_dropdown_element = {
+      padding: 2
+    };
 
     for (var i = 0; i < this.props.DropdownElementNames.length; i++) {
       Elements.push(
@@ -53,33 +73,43 @@ class Dropdown extends Component {
           name={this.props.DropdownElementNames[i].name}
           link={this.props.DropdownElementNames[i].link_}
           key={i.toString()}
+          style={style_dropdown_element}
         />
       );
     }
     var style_not_visible = {
       display: 'none',
       position: 'absolute',
-      zIndex: 1,
-      backgroundColor: '#fff',
-      padding: 30
+      zIndex: 1
     };
     var style_visible = {
       position: 'absolute',
       zIndex: 1,
       backgroundColor: '#fff',
-      padding: 15,
-      top: -90
+      padding: 10,
+      top: -100,
+      borderStyle: 'solid',
+      borderWidth: 0.2,
+      borderColor: '#000'
     };
 
     if (this.state.isClicked) {
       return (
         <div style={{ position: 'relative' }}>
           <div style={style_not_visible}>
+
             {Elements}
           </div>
-          <h3 onClick={this.onClick}>
+          <a
+            href=""
+            onClick={this.onClick}
+            onBlur={this.onBlur}
+            ref={input => {
+              this.button_focus = input;
+            }}
+          >
             Dropdown List{' '}
-          </h3>
+          </a>
         </div>
       );
     } else {
@@ -88,9 +118,17 @@ class Dropdown extends Component {
           <div style={style_visible}>
             {Elements}
           </div>
-          <h3 onClick={this.onClick}>
+
+          <a
+            href=""
+            onClick={this.onClick}
+            onBlur={this.onBlur}
+            ref={input => {
+              this.button_focus = input;
+            }}
+          >
             Dropdown List
-          </h3>
+          </a>
         </div>
       );
     }
