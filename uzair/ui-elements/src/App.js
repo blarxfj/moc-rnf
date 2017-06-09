60714bd9eae700eb148c9ac7b731fba85916013b
abiduzz420 <abiduzair420@gmail.com>
2017-06-09 15:31:07 +0530

    popover static version

--- uzair/ui-elements/src/App.js
+++ uzair/ui-elements/src/App.js
@@ -7,15 +7,19 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div
-        style={{
-          display: 'flex',
-          position: 'relative'
-        }}
-        id="app"
-      >
-        <TabContainer />
-        <DropdownContainer />
+      // <div
+      //   style={{
+      //     display: 'flex',
+      //     position: 'relative'
+      //   }}
+      //   id="app"
+      // >
+      //   <TabContainer />
+      //   <DropdownContainer />
+      // </div>
+      <div style={{ margin: '50px', display: 'flex' }}>
+        <button>Popover on Right</button>
+        <div>Building a popover on right in React</div>
       </div>
     );
   }
